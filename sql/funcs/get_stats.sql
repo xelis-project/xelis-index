c14e9a246ca67e220c59cb7ebdbca68fb70da365
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-03-12 00:12:48 -0500

    get_stats add block_count and remove supply (use reward)

--- sql/funcs/get_stats.sql
+++ sql/funcs/get_stats.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION get_stats("interval" text)
 RETURNS TABLE (
     "time" timestamp without time zone,
+    "block_count" bigint,
     "avg_difficulty" numeric,
     "sum_size" numeric,
     "avg_block_size" numeric,
-    "count_tx" bigint,
-    "sum_supply" numeric,
+    "tx_count" bigint,
     "avg_block_time" numeric,
     "sum_block_fees" numeric,
     "avg_block_fees" numeric,
@@ -15,11 +15,11 @@ RETURNS TABLE (
 BEGIN
   RETURN QUERY
     SELECT date_trunc(interval, to_timestamp(b."timestamp" / 1000) AT TIME ZONE 'UTC') AS "time",
+          count(b."hash") AS "block_count",
           round(avg(b."difficulty"), 2) AS "avg_difficulty",
           sum(b."size") AS "sum_size",
           round(avg(b."size"), 2) AS "avg_block_size",
-          count(t."hash") AS "count_tx",
-          sum(b."supply") AS "sum_supply",
+          count(t."hash") AS "tx_count",
           round(avg((nb."timestamp"-b."timestamp") / 1000), 2) AS "avg_block_time",
           sum(b."total_fees") AS "sum_block_fees",
           ceil(avg(b."total_fees")) AS "avg_block_fees",
