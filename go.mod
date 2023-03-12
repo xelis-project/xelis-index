81b5b67b7fd8b6aad58c96dc92f2a03fc232341a
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-03-12 13:48:13 -0400

    adding cli commands

--- go.mod
+++ go.mod
@@ -5,9 +5,12 @@ go 1.19
 require github.com/go-pg/pg/v10 v10.11.0
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/creachadair/jrpc2 v0.44.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xelis-project/xelis-go-sdk v0.0.0-20230309020418-b08f8626b838 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 )
 
@@ -17,6 +20,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/urfave/cli/v2 v2.25.0
 	github.com/vmihailenco/bufpool v0.1.11 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
--- main.go
+++ main.go
@@ -3,26 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/xelis-project/xelis-index/indexer"
 	"github.com/xelis-project/xelis-index/statements"
 
+	"github.com/urfave/cli/v2"
 	xelisDaemon "github.com/xelis-project/xelis-go-sdk/daemon"
 )
 
-func main() {
-	instance, err := indexer.LoadInstance(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
+var instance *indexer.Instance
 
-	defer instance.Close()
-
+func index() error {
 	var blockScan = indexer.BlockScan{Daemon: instance.Daemon}
 
 	scanHeight, err := instance.GetScanHeight()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	onBlock := func(block *xelisDaemon.Block) error {
@@ -86,6 +83,75 @@ func main() {
 		},
 	})
 
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func execsql(filePath string) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	tx, err := instance.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(string(data))
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "xelis-index",
+		Usage: "Xelis Index CLI for indexing the blockchain",
+		Before: func(ctx *cli.Context) error {
+			newInstance, err := indexer.LoadInstance(".env")
+			instance = newInstance
+			return err
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "index",
+				Usage: "scan / listen to blockchain and index the database",
+				Action: func(ctx *cli.Context) error {
+					return index()
+				},
+			},
+			{
+				Name:  "exec-sql",
+				Usage: "exec sql file (set seeds or migrations file)",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "filePath",
+						Required: true,
+					},
+				},
+				Action: func(ctx *cli.Context) error {
+					filePath := ctx.String("filePath")
+					return execsql(filePath)
+				},
+			},
+		},
+		After: func(ctx *cli.Context) error {
+			return instance.Close()
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
